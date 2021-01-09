025784a0859013d98d4deff9e4d085218d837e44
vuducduong <vuducduong193@gmail.com>
2021-01-09 13:09:28 +0700

    add file

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -57,9 +57,11 @@ class MovieController extends Controller
                 $request->video->move(public_path('videos'), $videoName);
                 $movie->video = $videoName;
                 $movie->save();
-
                 foreach ($request->actor as $actor1) {
                     Actor::find($actor1)->movies()->attach($movie->id);
+                }
+                foreach ($request->category as $val) {
+                    Category::find($val)->movies()->attach($movie->id);
                 }
 
             } catch (\Exception $e) {
@@ -96,8 +98,9 @@ class MovieController extends Controller
         $movie = Movie::findOrFail($id);
         $country = Country::all();
         $director = Director::all();
-
-        return view('Movies.edit', compact('movie', 'country', 'director'));
+        $actors= Actor::all();
+        $categories = Category::all();
+        return view('Movies.edit', compact('movie', 'country', 'director','categories','actors'));
     }
 
 
@@ -120,13 +123,13 @@ class MovieController extends Controller
                 $request->video->move(public_path('videos'), $videoName);
                 $movie->video = $videoName;
                 $movie->save();
-
                 foreach ($request->actor as $value) {
                     Actor::find($value)->movies()->attach($movie->id);
                 }
-                foreach ($request->category as $value) {
-                    Category::find($value)->movies()->attach($movie->id);
+                foreach ($request->category as $val) {
+                    Category::find($val)->movies()->attach($movie->id);
                 }
+
 
 
             } catch (\Exception $e) {
@@ -158,11 +161,12 @@ class MovieController extends Controller
     public function destroy($id)
     {
         $movies = Movie::find($id);
-
+//        $movies->categories()->detach();
+//        $movies->actors()->detach();
+        $movies->categories()->sync([]);
+        $movies->actors()->sync([]);
         $movies->delete();
-        foreach($request->actor as $actor1){
-            Actor::find($actor1)->movies()->detach();
-        }
+
         return redirect()->route('movies.list');
     }
 
