b95f37d775569c60fcfa93b40446e85d38b183d3
vuducduong <vuducduong193@gmail.com>
2021-01-09 10:48:28 +0700

    add file

--- resources/views/Movies/create.blade.php
+++ resources/views/Movies/create.blade.php
@@ -6,24 +6,22 @@
                 <h1>Thêm Mới phim</h1>
             </div>
             <div class="col-12">
-                <form method="post" action="{{ route('movies.store') }}"  enctype="multipart/form-data">
+                <form method="post" action="{{ route('movies.store') }}" enctype="multipart/form-data">
                     @csrf
-                    <div  class="form-group">
+                    <div class="form-group">
                         <label>Tên Phim</label>
-                        <input  type="text" class="form-control" name="name"  required>
+                        <input type="text" class="form-control" name="name" required>
                     </div>
 
                     <div class="form-group">
                         <label>Year</label>
-                        <input type="number" class="form-control" name="year"  required>
+                        <input type="number" class="form-control" name="year" required>
                     </div>
 
                     <div class="form-group">
                         <label>Time</label>
                         <input type="number" class="form-control" name="time" required>
                     </div>
-
-
 
 
                     <div class="form-group">
@@ -36,8 +34,6 @@
                     </div>
 
 
-
-
                     <div class="form-group">
                         <label> Đạo Diễn</label>
                         <select class="form-control" name="director_id">
@@ -48,45 +44,36 @@
                     </div>
 
 
-
-
-                    <td>
-                        @foreach($actors as $actor)
-                            <div class="form-check">
-                                <input
-                                    class="form-check-input"
-                                    name="actor[]"
-                                    type="checkbox"
-                                    value="{{ $actor->id }}"
-                                    id="flexCheckDefault"
-                                />
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    {{ $actor->name }}
-                                    <img class="img-responsive" src="{{ asset('uploads/'.$actor->image) }}" border="1" height="64" width="64" alt=""/>
-                                </label>
-                            </div>
-                        @endforeach
-{{--                        @if($errors->any())--}}
-{{--                            <p class="alert-danger my-sm-4">{{ $errors->first('author') }}</p>--}}
-{{--                        @endif--}}
-                    </td>
-
-
+                    <div class="form-group">
+                        <label>Diễn Viên</label>
+                        <div class="form-group">
+                            <select class="form-control" id="exampleFormControlSelect1" name="actor[]">
+                                @foreach($actors as $actor)
+                                    <option value="{{$actor->id}}">{{$actor->name}}</option>
+                                @endforeach
+                            </select>
+                            </td>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Thể Loại</label>
+                        <select class="form-control" id="exampleFormControlSelect1" name="category[]">
+                            @foreach($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                        </td>
+                    </div>
                     <div class="form-group">
                         <label>Ảnh </label>
-                       <input type="file" class="form-control" name="image" >
+                        <input type="file" class="form-control" name="image">
                     </div>
-
-
 
 
                     <div class="form-group">
                         <label>Video</label>
-                        <input type="file" class="form-control-file" name="video" >
+                        <input type="file" class="form-control-file" name="video">
                     </div>
-
-
-
 
 
                     <button type="submit" class="btn btn-primary">Thêm mới</button>
